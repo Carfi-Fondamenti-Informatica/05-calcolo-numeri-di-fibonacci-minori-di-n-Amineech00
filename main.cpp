01aea8bbc02a9960ad194513ff418907408b6dff
Amineech00 <118432146+Amineech00@users.noreply.github.com>
2022-11-28 10:00:34 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,7 +2,7 @@
 using namespace std;
 
 int main() {
-    int n=0 , a=1 ,b=1 , c=0
+    int n=0 , a=1 ,b=1 , c=0;
     cin >> n;
     cout << a << endl << b << endl;
     for ( int i = 0; i <= n; i++){
